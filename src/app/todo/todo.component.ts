b75d91be289c7c1c0d92b14073500b5f84341075
ekrem-kocak <ekrem-kocak@outlook.com>
2021-09-19 13:43:58 +0300

    3)publishing commit

--- src/app/todo/todo.component.ts
+++ src/app/todo/todo.component.ts
@@ -9,15 +9,69 @@ import { TodoItem } from '../Models/TodoItem';
 })
 export class TodoComponent {
 
-  constructor() { }
+  constructor() {
+    this.model.items = this.GetItemsFromLS();
+  }
+
+  displayAll: boolean = false;
 
   model = new Model();
 
-  GetName(){
+  inputText: string = "";
+  message: string;
+
+  AddItem() {
+    if (this.inputText != "") {
+      let data = new TodoItem(this.inputText, false);
+      this.model.items.push(data);
+
+      let items = this.GetItemsFromLS();
+      items.push(data);
+
+      localStorage.setItem('items', JSON.stringify(items));
+      this.inputText = '';
+    } else {
+      alert("bilgi giriniz");
+    }
+  }
+  DeleteItem(desc: string) {
+    var item = this.model.items.find(i => i.description == desc);
+    if (item != null) {
+      this.model.items.splice(this.model.items.indexOf(item), 1);
+    }
+  }
+
+  OnActionChanged(item: TodoItem) {
+    let items = this.GetItemsFromLS();
+    localStorage.clear();
+
+    items.forEach(i => {
+      if (i.description == item.description) {
+        i.action = item.action;
+      }
+    });
+    localStorage.setItem('items', JSON.stringify(items));
+  }
+  GetName() {
     return this.model.name;
   }
-  GetItems(){
-    return this.model.items;
+  GetItems() {
+    if (this.displayAll) {
+      return this.model.items;
+    } else {
+      return this.model.items.filter(i => i.action === false);
+    }
+  }
+  GetItemsFromLS() {
+    let items: TodoItem[] = [];
+    let data = localStorage.getItem('items');
+    if (data != null) {
+      items = JSON.parse(data);
+    }
+    return items;
+  }
+  DisplayCompletedCount() {
+    return this.model.items.filter(i => i.action).length;
   }
 
 }
